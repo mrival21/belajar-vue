570ea714a6e0eeb0081203beea347bbcee8771f9
mrival21 <rivalnugraha656@gmail.com>
2023-08-07 01:46:00 +0700

    belajarvuexmodule

---
+++ belajar-vuex-module/src/store/modules/donatur.js
@@ -0,0 +1,19 @@
+const donatur = {
+    namespaced: true,
+    state: {
+        listDonatur: [
+            { name: "rival"},
+            { name: "riska"},
+            { name: "roh halus"},
+            { name: "huslam"},
+        ],
+    },
+    mutations: {
+        //NONE
+    },
+    actions: {
+        //NONE
+    },
+};
+
+export default donatur;
